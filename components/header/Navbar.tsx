bfa0accbcc9210a2b51c2c4182fae0a5d2c99052
waly <digitalelitepro@gmail.com>
2025-05-09 10:47:33 +0000

    Bis Connect commit 9

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
           />
       </div>
       <div className='flex flex-col justify-end place-items-end'>
-        <h1>Hotline</h1>
-        <p>+221 33 849 62 62</p>
+        <h4 className='text-lg font-bold text-blue-900'>Hotline</h4>
+        <span className='bg-blue-950  py-1 px-3 rounded-lg  text-white font-semibold transition-all duration-900 ease-in-out animate-bounce mt-1 hover:animate-none hover:scale-105 hover:text-yellow-400'>+221 33 849 62 62</span>
       </div>
     </div>
   )
