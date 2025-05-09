9d21e0d11bace1e638fff3ad7876b61420126740
waly <digitalelitepro@gmail.com>
2025-05-09 12:08:24 +0000

    Bis connect commit 11

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-27 sm:h-23 flex flex-col justify-center items-center  space-y-1 bg-blue-950 text-white '>
-       <p className='text-[9px] text-center'>Raphael Waly SENGHOR  </p>
-       <p  className='text-[9px] text-center'>Consultant en Monétique - Developpeur web stack - Formateur  - consultant en transformation digitale </p>
-       <h2 className='text-sm text-center pt-1'>Siège social : Almadies Zone 12, niveau rond-point,
+       <p className='text-sm text-center'>Raphael Waly SENGHOR  </p>
+       <p  className='text-sm text-center'>Consultant en monétique - Developpeur web fullstack - Formateur  - consultant en transformation digitale </p>
+       <h2 className='text-[9px] text-center pt-1'>Siège social : Almadies Zone 12, niveau rond-point,
        face route King Fahd, Dakar-Sénégal - B.P : 3381 - Dakar</h2>
   
         <p className='mb-1 text-[7px] font-semibold'>© 2024 Banque Islamique du Sénégal • Tous droits réservés</p>
