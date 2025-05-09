84f21b1346fdfd5bc7527fc4a561dc0e0da7dc32
waly <digitalelitepro@gmail.com>
2025-05-09 12:34:51 +0000

    Bis connect commit 15

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link  from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,12 +6,15 @@ const Navbar = () => {
   return (
     <div className='h-19 fixed bg-gradient-to-b from-white to-cyan-50 inset-0 flex justify-between items-center p-2 shadow-sm shadow-slate-200/50 opacity-100  '>
       <div>
-          <Image
+        <Link href='/'>   
+         <Image
             src='https://bis-bank.com/wp-content/uploads/2022/07/logo-bis4.svg'
             width={180}
             height={100}
             alt='logo bis bank'
           />
+        </Link>
+      
       </div>
       <div className='flex flex-col justify-end place-items-end'>
         <h4 className='text-lg font-bold text-blue-900'>Hotline</h4>
